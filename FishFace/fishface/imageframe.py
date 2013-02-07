4ca5ae756d620d99653f4134e1078cee534435df
Wil Langford <github@langford.ws>
2013-02-06 20:04:51 -0800

    Almost done re-implementing from fish-tracker.

--- FishFace/fishface/imageframe.py
+++ FishFace/fishface/imageframe.py
@@ -30,13 +30,13 @@ class Frame:
                 self.cvmat = image
 
         elif(type(image)==np.ndarray):
-            self.fromarray(image)
+            self.fromArray(image)
 
         elif(type(image)==str):
             self.setImageFromFile(image)
         
         else:
-            raise ImageInitError("newImage requires a cvMat, numpy array, or filename string.")
+            raise ImageInitError("setImage requires a cvMat, numpy array, or filename string, but I see a {}".format(type(image)))
 
         self.mask = None
 
@@ -47,19 +47,25 @@ class Frame:
         else:
             raise ImageInitError("File not found: {}".format(filename))
 
-    def fromarray(self,image):
-        """Convert Numpy array to CvMat."""
+    def fromArray(self,image):
+        """Convert Numpy array to my CvMat and store as my image."""
         if image.dtype==np.uint8:
             self.cvmat = cv.fromarray(image)
         else:
             raise ImageInitError("Image must have 8 bits per channel.")
+    
+    def toArray(self):
+        """Convert my image from CvMat to Numpy array and return it."""
+        return np.asarray(self.cvmat)
+
 
     def saveImageToFile(self, filename):
         """Save the image to a file."""
-        cv.SaveImage(filename, self.cvmat)
+        cv.SaveImage(filename, self.cvmat)                
     
     def onScreen(self, scaleDownFactor=2):
-        """I need something to display these things for debugging."""
+        """I need something to display these things for debugging. This uses
+        Tkinter to display in a no-frills, click-to-dismiss window."""
         
         root = tk.Tk()
         root.title("image display - click to close")
@@ -96,14 +102,65 @@ class Frame:
         
         root.mainloop()
 
-    def findOutlineInMask(self, mask=None, returnCopy=False):
-        pass
+    def findEdges(self, returnMat=False, thickenEdges=True, thickener=3):
+        """Uses OpenCV's Canny filter implementation to find edges."""
+        
+        out = self.blankCopy(mode="L")
+        
+        cv.Canny(self.cvmat, out, 50, 100, 3)
+        
+        if thickenEdges:
+            se = cv.CreateStructuringElementEx(2*thickener+1, 2*thickener+1,
+                                                thickener, thickener,
+                                                 cv.CV_SHAPE_ELLIPSE)
+            cv.Dilate(out,out,se)
+        
+        if returnMat:
+            return out
+        else:
+            self.mask = out
+        
+    def mask2image(self):
+        self.setImage(self.mask)
+    
+    def image2mask(self):
+        self.mask = self.grayImage(returnMat=True)   
+        
+    def blankMask(self):
+        self.mask = self.blankCopy()
 
-    def grayImage(self, returnCopy=False, method=0):
-        """If my image is grayscale, return it.  Otherwise, convert it to grayscale and
-        overwrite my image with the newly gray image only if requested."""
+    def blankCopy(self, mode='L'):
+        if mode=="RGB":
+            mode=cv.CV_8UC3
+        else:
+            mode=cv.CV_8UC1
+        
+        blank = cv.CreateMat(self.cvmat.rows, self.cvmat.cols, mode)
+        cv.SetZero(blank)
+        return blank
+
+    def deltaImage(self, calImage, threshold=40, returnMat=False):
+        """Subtracts the provided calImage from my image, then thresholds
+        and stores/returns the result."""
+        
+        cg = calImage.grayImage(returnMat=True)
+        sg = self.grayImage(returnMat=True)
+        
+        diff = self.blankCopy(mode="L")
+        
+        cv.AbsDiff(cg,sg,diff)
+        
+        if returnMat:
+            return diff
+        else:
+            self.cvmat = diff
+            return None
+            
+    def grayImage(self, returnMat=False, method=0):
+        """If my image is grayscale, return it.  Otherwise, convert to grayscale
+        and store/return the result."""
         if self.cvmat.channels==1:
-            if returnCopy==True:
+            if returnMat:
                 return self.cvmat
             else:
                 return None
@@ -111,7 +168,7 @@ class Frame:
             gray_mat = cv.CreateMat(self.cvmat.rows, self.cvmat.cols, cv.CV_8UC1)                
             cv.CvtColor(self.cvmat, gray_mat, cv.CV_RGB2GRAY)
             
-            if returnCopy:
+            if returnMat:
                 return gray_mat
             else:
                 self.cvmat = gray_mat
@@ -119,6 +176,11 @@ class Frame:
         else:
             raise ImageProcessError("Image did not have either 1 or 3 channels. Can't convert to grayscale.") 
                 
+    def largestObjectInMask(self, mask=None, returnMat=False):
+        pass
+
+
+
 
 class ImageInitError(Exception):
     pass
