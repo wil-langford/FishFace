414f4ef8318a8b6f0d679c43476c735b5e5cc958
Wil Langford <github@langford.ws>
2013-02-07 20:20:58 -0800

    Probably should have committed more today.
    There were too many changes to summarize cleanly in this commit.

--- FishFace/fishface/imageframe.py
+++ FishFace/fishface/imageframe.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import Tkinter as tk
 import Image, ImageTk
+import copy
 
 ### Both lines below do the same thing in the actual Python interpreter,
 ### but PyDev/Eclipse wants the second one for autocomplete and code
@@ -15,6 +16,7 @@ import cv2
 class Frame:
     def __init__(self, image):
         self.setImage(image)
+        self.roi = [0,0,0,0]
     
     def setImage(self, image, copyArray=True):
         """Object is getting a new image.  If it's already a numpy.array with
@@ -45,7 +47,7 @@ class Frame:
         """Save the image to a file."""
         cv2.imwrite(filename, cv2.cvtColor(self.array, cv.CV_BGR2RGB))                
     
-    def onScreen(self, scaleDownFactor=2, message = None):
+    def onScreen(self, scaleDownFactor=1, message = None):
         """I need something to display these things for debugging. This uses
         Tkinter to display in a no-frills, click-to-dismiss window."""
         
@@ -78,16 +80,29 @@ class Frame:
         out = cv2.Canny(self.array, 50, 100, apertureSize=3)
         
         if thickenEdges:
-            se = self.slider(thickener, shape="circ")
+            se = self.kernel(thickener, shape="circ")
             out = cv2.dilate(out,se)
         
         if returnArray:
             return out
         else:
-            self.array = out
+            self.setImage(out)
 
     def shallowcopy(self):
         return self.__copy__()
+
+    def setRoiFromContour(self,contour):
+        maxes = np.amax(contour, axis=1)[0,0]
+        mins  = np.amin(contour, axis=1)[0,0]
+        
+        self.roi = [mins[1], maxes[1], mins[0], maxes[0]]
+
+    def roiImage(self):
+        return self.array[self.roi[0]:self.roi[1],
+                          self.roi[2]:self.roi[3]]
+
+    def roi2image(self):
+        self.setImage(self.roiImage())
 
     def copy(self):
         return self.__deepcopy__()
@@ -98,17 +113,17 @@ class Frame:
         if mode=="L":
             return np.zeros(self.array.shape[0:1])
 
-    def deltaImage(self, calImage, returnArray=False):
+    def deltaImage(self, calImageFrame, returnArray=False):
         """Finds the absolute difference between the calImage and my array,
         then stores/returns the result."""
         
-        diff = cv2.absdiff(calImage.grayImage(returnArray=True),
+        diff = cv2.absdiff(calImageFrame.grayImage(returnArray=True),
                            self.grayImage(returnArray=True))
         
         if returnArray:
             return diff
         else:
-            self.array = diff
+            self.setImage(diff)
             return None
 
     def threshold(self, threshold, returnArray=False):
@@ -120,7 +135,7 @@ class Frame:
         if returnArray:
             return out
         else:
-            self.array = out
+            self.setImage(out)
             return None
                    
     def grayImage(self, returnArray=False, method=0):
@@ -152,10 +167,10 @@ class Frame:
         if returnArray:
             return gray
         else:
-            self.array = gray
+            self.setImage(gray)
             return None 
 
-    def slider(self, radius=3, shape="circ"):
+    def kernel(self, radius=3, shape="circ"):
         if shape=="circ":
             shp = cv2.MORPH_ELLIPSE
         elif shape=="cross":
@@ -163,27 +178,94 @@ class Frame:
         elif shape=="rect":        
             shp = cv2.MORPH_RECT
         else:
-            raise ImageProcessError("Couldn't create a slider with shape: {}".format(shape))
+            raise ImageProcessError("Couldn't create a kernel with shape: {}".format(shape))
         return cv2.getStructuringElement(shp, (radius*2+1, radius*2+1) )
                 
-    def findLargestObjectContours(self, sliderRadius=3, iterClosing=3, iterOpening=3):
-        
-        out = np.copy(self.array)
-        
-        # dilation followed by erosion fills in holes
-        # this process is called closing
-        se = self.slider(sliderRadius)
-        out = cv2.dilate(out, kernel=se, iterations=iterClosing)        
-        out = cv2.erode(out, kernel=se, iterations=iterClosing)
+    def toTheBones(self, skelKernelRadius=1, skelKernelShape='circ', returnArray=False):
+        src = np.copy(self.array)
+        size = np.size(src)
+        out = np.zeros(src.shape,np.uint8)
 
-        # erosion followed by dilation removes smaller objects
-        # this process is called opening
-        se = self.slider(sliderRadius)
-        out = cv2.erode(out, kernel=se, iterations=iterOpening)
-        out = cv2.dilate(out, kernel=se, iterations=iterOpening)        
+        se = self.kernel(radius=skelKernelRadius, shape=skelKernelShape)
+        
+        complete = False
+        
+        while(not complete):
+            eroded = cv2.erode(src, se)
+            eph = cv2.dilate(eroded, se)
+            eph = cv2.subtract(src,eph)
+            out = cv2.bitwise_or(out,eph)
+            src = np.copy(eroded)
+        
+            zeros = size - cv2.countNonZero(src)
+            if zeros == size:
+                complete = True
+
+        if returnArray:
+            return out
+        else:
+            self.setImage(out)
+            return None
+
+    def dilate(self, kernel, dilIter, returnArray=False):
+        arr = np.copy(self.array)
+        
+        arr = cv2.dilate(arr, kernel=kernel, iterations=dilIter)
+
+        if returnArray:
+            return arr
+        else:
+            self.setImage(arr)
+            return None
+
+    def erode(self, kernel, erIter, returnArray=False):
+        arr = np.copy(self.array)
+
+        arr = cv2.erode(arr, kernel=kernel, iterations=erIter)
+
+        if returnArray:
+            return arr
+        else:
+            self.setImage(arr)
+            return None
+
+    def binOpening(self, kernelRadius=3, kernelShape='circ', iterations=1, returnArray=False):
+        out = self.copy()
+        
+        se = self.kernel(radius=kernelRadius, shape=kernelShape)
+
+        out.erode(se, iterations)
+        out.dilate(se, iterations)
+
+        if returnArray:
+            return out
+        else:
+            self.setImage(out.array)
+            return None
+
+    def binClosing(self, kernelRadius=3, kernelShape='circ', iterations=1, returnArray=False):
+        out = self.copy()
+        
+        se = self.kernel(radius=kernelRadius, shape=kernelShape)
+
+        out.dilate(se, iterations)
+        out.erode(se, iterations)
+
+        if returnArray:
+            return out
+        else:
+            self.setImage(out.array)
+            return None
+
+    def findLargestObjectContours(self, kernelRadius=3, iterClosing=3, iterOpening=3, setRoi=True):
+        
+        out = self.copy()
+        
+        out.binClosing(kernelRadius=kernelRadius, iterations=iterClosing)
+        out.binOpening(kernelRadius=kernelRadius, iterations=iterOpening)
 
         # the [0] at the end is because we don't care about the hierarchy
-        contours = cv2.findContours(out,
+        contours = cv2.findContours(out.array,
                                     mode=cv2.RETR_EXTERNAL,
                                     method=cv2.CHAIN_APPROX_SIMPLE
                                     )[0]
@@ -191,22 +273,90 @@ class Frame:
         areas = [cv2.contourArea(ctr) for ctr in contours]
         max_contour = [contours[areas.index(max(areas))]]
 
+        if setRoi:
+            self.setRoiFromContour(max_contour)
+
         return max_contour
 
-    def outlineLargestObjectWithContours(self, contours, lineColor=(255,100,100), lineThickness=3):
+    def outlineLargestObjectWithContours(self, contours, lineColor=(255,0,255), lineThickness=3, filledIn=True, adjustForRoi=False):
+
+        if filledIn:
+            lineThickness = -abs(lineThickness)
+
+        if adjustForRoi:
+            contours = np.array(contours) + np.array([[self.roi[0], self.roi[2]]])
+
         cv2.drawContours(image=self.array,
                          contours=contours,
                          contourIdx=-1,
                          color=lineColor,
                          thickness=lineThickness)
+
+    def circlesAtPoints(self, points, lineColor=(255,0,255), lineThickness=3, filledIn=True, circleRadius=3, adjustForRoi=False):
+
+        if filledIn:
+            lineThickness = -abs(lineThickness)
+
+        points = np.roll(points, 1, axis=2)
+
+        if adjustForRoi:
+            xadj=self.roi[0]
+            yadj=self.roi[2]
+        else:
+            xadj=0
+            yadj=0
+                    
+        points = [(int(x[0,1]+yadj),int(x[0,0]+xadj)) for x in points]
+
+        if self.array.ndim==2:
+            lineColor=sum(lineColor)//3
+
+        for point in points:
+            cv2.circle(img=self.array,
+                       center=tuple(point),
+                       radius=circleRadius,
+                       color=lineColor)
+
+    def drawOutlineAroundLargestObject(self, calImageFrame, threshold=50, kernelRadius=3, lineColor=(255,0,255), lineThickness=3, filledIn=True, adjustForRoi=False):
+        child = self.copy()
+        
+        child.deltaImage(calImageFrame)
+        child.threshold(threshold)
+        
+        self.outlineLargestObjectWithContours(child.findLargestObjectContours(kernelRadius=kernelRadius),
+                                              lineColor, lineThickness, filledIn)
+
+    def isolateLargestBlob(self, calImageFrame, threshold=50, kernelRadius=3, returnArray=False, lineColor=(255,0,255), lineThickness=3, filledIn=True, adjustForRoi=False):        
+        out = self.copy()
+        
+        out.deltaImage(calImageFrame)
+        out.threshold(threshold)        
+        ctr = out.findLargestObjectContours(kernelRadius=kernelRadius)
+    
+        out.setImage(out.blankCopy())    
+        out.outlineLargestObjectWithContours(ctr, lineColor, lineThickness, filledIn=True, adjustForRoi=adjustForRoi)
+                        
+        out.roi2image()
+        
+
+
+        if returnArray:
+            return out.array
+        else:
+            self.setImage(out.array)
+            self.roi = out.roi
+            return None
+
         
     def __copy__(self):
-        newFrame = Frame(self.array)
+        newFrame = Frame(self.array, copyArray=False)
+        newFrame.roi = copy.copy(self.roi)
         return newFrame
 
     def __deepcopy__(self, memodic=None):
         newFrame = Frame(self.blankCopy())
         newFrame.array = np.copy(self.array)
+        newFrame.roi = copy.deepcopy(self.roi, memodic)
         return newFrame
 
 
