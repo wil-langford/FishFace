0d03494e154eb5090e3f132b5eb0c94f528c82bd
Wil Langford <wil.langford+homework@gmail.com>
2013-02-15 15:13:54 -0800

    Added cropping features to command line tool.

--- FishFace/bin/FishFace.py
+++ FishFace/bin/FishFace.py
@@ -47,20 +47,56 @@ def main(arguments):
                         metavar="RGB_COLOR", default=(255,0,255),
                         help = 'An RGB 3-tuple specifying the color to draw annotations.',
                         action='store')
-    parser.add_argument('-O','--outline-foreground-object', dest='outline',
-                        help = 'A command that outlines the foreground object.',
-                        action='store_true')
+    parser.add_argument('--crop-input-box', dest='crop_input_box', type=str,
+                        metavar="CROP_INPUT_BOX", default=None,
+                        help = 'The area of the input image to use.  Of the form "Y_MIN x X_MIN - Y_MAX x X_MAX. Example: "0x0 - 100x200" uses only the first 200 pixels of the first 100 rows of the image.',
+                        action='store')
+
     parser.add_argument('-Q','--supress-output', dest='quiet',
                         help = 'A command to supress all output for testing purposes.',
                         action='store_true')
 
+
+    parser.add_argument('-C','--crop-to-foreground-object', dest='crop',
+                        help = 'A command that crops the image to the foreground object.',
+                        action='store_true')
+    parser.add_argument('-O','--outline-foreground-object', dest='outline',
+                        help = 'A command that outlines the foreground object.',
+                        action='store_true')
+
     args = parser.parse_args(arguments)
+
+    if args.crop_input_box:
+        mins,maxes = args.crop_input_box.split('-')
+        mins = [int(x) for x in mins.split("x")]
+        maxes = [int(x) for x in maxes.split("x")]
+        crop_box = (mins[0], mins[1], maxes[0], maxes[1])
+
+    if args.crop:
+        im_in = imageframe.Frame(args.infile)
+        im_cal = imageframe.Frame(args.calfile)
+
+        if crop_box:
+            im_in.crop(crop_box)
+            im_cal.crop(crop_box)
+
+        im_in.cropToLargestBlob(im_cal)
+
+        if not args.quiet:        
+            if args.outfile:
+                im_in.saveImageToFile(args.outfile)
+            else:
+                im_in.onScreen(1, "Isolated largest object versus provided calibration image.")        
     
     if args.outline:
         im_in = imageframe.Frame(args.infile)
         im_cal = imageframe.Frame(args.calfile)
+
+        if crop_box != (-1):
+            im_in.crop(crop_box)
+            im_cal.crop(crop_box)
         
-        im_in.drawOutlineAroundLargestObject(calImageFrame=im_cal,
+        im_in.drawOutlineAroundLargestBlob(calImageFrame=im_cal,
                                             threshold=args.threshold,
                                             kernelRadius=args.ksize,
                                             lineColor=args.color,
@@ -70,7 +106,7 @@ def main(arguments):
             if args.outfile:
                 im_in.saveImageToFile(args.outfile)
             else:
-                im_in.onScreen(1, "Outlined largest object versus provided calibration image.")        
+                im_in.onScreen(2, "Outlined largest object versus provided calibration image.")        
     
 
 if __name__ == '__main__':
