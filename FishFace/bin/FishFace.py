78a3bfd427b5317028afa917e060502f316b0569
Wil Langford <github@langford.ws>
2013-03-14 13:36:50 -0700

    Removed sinks pane from interface.  Not needed.

--- FishFace/bin/FishFace.py
+++ FishFace/bin/FishFace.py
@@ -43,25 +43,14 @@ class FishFaceApp(wx.Frame):
         splitter.SplitVertically(leftPanel, rightPanel)
         splitter.SetMinimumPaneSize(120)
 
-        # Left splitter (top panel from bottom panels)
+        # Left splitter (top panel from bottom panel)
         leftSplitter = wx.SplitterWindow(leftPanel, style=wx.SP_3D)
         self.sourcesPanel = wx.Panel(leftSplitter, style=wx.SUNKEN_BORDER)
-        bottomPanels = wx.Panel(leftSplitter)
-        leftSplitter.SplitHorizontally(self.sourcesPanel, bottomPanels)
-        leftSplitter.SetMinimumPaneSize(160)
-
-        # bottom splitter (mid panel from low panel)
-        bottomSplitter = wx.SplitterWindow(bottomPanels, style=wx.SP_3D)
-        self.chainsPanel = wx.Panel(bottomSplitter, style=wx.SUNKEN_BORDER)
-        self.sinksPanel = wx.Panel(bottomSplitter, style=wx.SUNKEN_BORDER)
-        bottomSplitter.SplitHorizontally(self.chainsPanel, self.sinksPanel)
-        bottomSplitter.SetMinimumPaneSize(80)
+        self.chainsPanel = wx.Panel(leftSplitter)
+        leftSplitter.SplitHorizontally(self.sourcesPanel, self.chainsPanel)
+        leftSplitter.SetMinimumPaneSize(120)
 
         # Sizers and such
-        bottomSizer = wx.BoxSizer(wx.HORIZONTAL)
-        bottomSizer.Add(bottomSplitter, 1, wx.EXPAND)
-        bottomPanels.SetSizer(bottomSizer)
-
         leftSizer = wx.BoxSizer(wx.HORIZONTAL)
         leftSizer.Add(leftSplitter, 1, wx.EXPAND)
         leftPanel.SetSizer(leftSizer)
@@ -86,14 +75,6 @@ class FishFaceApp(wx.Frame):
         chainsSizer.Add(self.chainsButtonBar, 0, wx.ALL)
         self.chainsPanel.SetSizer(chainsSizer)
 
-        # Sinks List
-        self.sinksList = wx.ListCtrl(self.sinksPanel)
-        self.sinksButtonBar = wx.Panel(self.sinksPanel)
-        sinksSizer = wx.BoxSizer(wx.VERTICAL)
-        sinksSizer.Add(self.sinksList, 1, wx.EXPAND)
-        sinksSizer.Add(self.sinksButtonBar, 0, wx.ALL)
-        self.sinksPanel.SetSizer(sinksSizer)
-
         # button bars' layouts
         sourcesBBS = wx.BoxSizer(wx.HORIZONTAL)
         sourcesBBS.Add(wx.Button(self.sourcesButtonBar, id=wx.ID_ADD, style=wx.BU_EXACTFIT))
@@ -107,21 +88,12 @@ class FishFaceApp(wx.Frame):
         chainsBBS.Add(wx.Button(self.chainsButtonBar, id=wx.ID_EDIT, style=wx.BU_EXACTFIT))
         self.chainsButtonBar.SetSizer(chainsBBS)
 
-        sinksBBS = wx.BoxSizer(wx.HORIZONTAL)
-        sinksBBS.Add(wx.Button(self.sinksButtonBar, id=wx.ID_ADD, style=wx.BU_EXACTFIT))
-        sinksBBS.Add(wx.Button(self.sinksButtonBar, id=wx.ID_REMOVE, style=wx.BU_EXACTFIT))
-        sinksBBS.Add(wx.Button(self.sinksButtonBar, id=wx.ID_EDIT, style=wx.BU_EXACTFIT))
-        self.sinksButtonBar.SetSizer(sinksBBS)
-
         # Set overall window geometry and show window
         self.SetSize((900, 600))
         self.Show(True)
 
         # Set initial splitter sash positions and gravities
-        leftSplitter.SetSashPosition(int(leftSplitter.GetSizeTuple()[1] / 3))
-        leftSplitter.SetSashGravity(0.3333)
-        bottomSplitter.SetSashGravity(0.5)
-        splitter.SetSashPosition(int(splitter.GetSizeTuple()[0] / 3))
+        splitter.SetSashPosition(int(splitter.GetSizeTuple()[0] / 4))
 
     def onExit(self, event):
 #        dialog = wx.MessageDialog(self, "Really close?", "Confirm Close", wx.OK | wx.CANCEL | wx.ICON_QUESTION)
