0bb867685383be7d2b506b1f6bc4ca5d6b05e88e
Wil Langford <github@langford.ws>
2013-03-13 18:52:52 -0700

    Panes are ready-to-go-ish.

--- FishFace/bin/FishFace.py
+++ FishFace/bin/FishFace.py
@@ -48,7 +48,7 @@ class FishFaceApp(wx.Frame):
         self.sourcesPanel = wx.Panel(leftSplitter, style=wx.SUNKEN_BORDER)
         bottomPanels = wx.Panel(leftSplitter)
         leftSplitter.SplitHorizontally(self.sourcesPanel, bottomPanels)
-        leftSplitter.SetMinimumPaneSize(80)
+        leftSplitter.SetMinimumPaneSize(160)
 
         # bottom splitter (mid panel from low panel)
         bottomSplitter = wx.SplitterWindow(bottomPanels, style=wx.SP_3D)
@@ -70,6 +70,49 @@ class FishFaceApp(wx.Frame):
         sizer.Add(splitter, 1, wx.EXPAND)
         self.SetSizer(sizer)
 
+        # Source Tree
+        self.sourcesTree = wx.TreeCtrl(self.sourcesPanel)
+        self.sourcesButtonBar = wx.Panel(self.sourcesPanel)
+        sourceSizer = wx.BoxSizer(wx.VERTICAL)
+        sourceSizer.Add(self.sourcesTree, 1, wx.EXPAND)
+        sourceSizer.Add(self.sourcesButtonBar, 0, wx.ALL)
+        self.sourcesPanel.SetSizer(sourceSizer)
+
+        # Chains List
+        self.chainsList = wx.ListCtrl(self.chainsPanel)
+        self.chainsButtonBar = wx.Panel(self.chainsPanel)
+        chainsSizer = wx.BoxSizer(wx.VERTICAL)
+        chainsSizer.Add(self.chainsList, 1, wx.EXPAND)
+        chainsSizer.Add(self.chainsButtonBar, 0, wx.ALL)
+        self.chainsPanel.SetSizer(chainsSizer)
+
+        # Sinks List
+        self.sinksList = wx.ListCtrl(self.sinksPanel)
+        self.sinksButtonBar = wx.Panel(self.sinksPanel)
+        sinksSizer = wx.BoxSizer(wx.VERTICAL)
+        sinksSizer.Add(self.sinksList, 1, wx.EXPAND)
+        sinksSizer.Add(self.sinksButtonBar, 0, wx.ALL)
+        self.sinksPanel.SetSizer(sinksSizer)
+
+        # button bars' layouts
+        sourcesBBS = wx.BoxSizer(wx.HORIZONTAL)
+        sourcesBBS.Add(wx.Button(self.sourcesButtonBar, id=wx.ID_ADD, style=wx.BU_EXACTFIT))
+        sourcesBBS.Add(wx.Button(self.sourcesButtonBar, id=wx.ID_REMOVE, style=wx.BU_EXACTFIT))
+        sourcesBBS.Add(wx.Button(self.sourcesButtonBar, id=wx.ID_EDIT, style=wx.BU_EXACTFIT))
+        self.sourcesButtonBar.SetSizer(sourcesBBS)
+
+        chainsBBS = wx.BoxSizer(wx.HORIZONTAL)
+        chainsBBS.Add(wx.Button(self.chainsButtonBar, id=wx.ID_ADD, style=wx.BU_EXACTFIT))
+        chainsBBS.Add(wx.Button(self.chainsButtonBar, id=wx.ID_REMOVE, style=wx.BU_EXACTFIT))
+        chainsBBS.Add(wx.Button(self.chainsButtonBar, id=wx.ID_EDIT, style=wx.BU_EXACTFIT))
+        self.chainsButtonBar.SetSizer(chainsBBS)
+
+        sinksBBS = wx.BoxSizer(wx.HORIZONTAL)
+        sinksBBS.Add(wx.Button(self.sinksButtonBar, id=wx.ID_ADD, style=wx.BU_EXACTFIT))
+        sinksBBS.Add(wx.Button(self.sinksButtonBar, id=wx.ID_REMOVE, style=wx.BU_EXACTFIT))
+        sinksBBS.Add(wx.Button(self.sinksButtonBar, id=wx.ID_EDIT, style=wx.BU_EXACTFIT))
+        self.sinksButtonBar.SetSizer(sinksBBS)
+
         # Set overall window geometry and show window
         self.SetSize((900, 600))
         self.Show(True)
@@ -78,7 +121,7 @@ class FishFaceApp(wx.Frame):
         leftSplitter.SetSashPosition(int(leftSplitter.GetSizeTuple()[1] / 3))
         leftSplitter.SetSashGravity(0.3333)
         bottomSplitter.SetSashGravity(0.5)
-        splitter.SetSashPosition(int(splitter.GetSizeTuple()[0] / 4))
+        splitter.SetSashPosition(int(splitter.GetSizeTuple()[0] / 3))
 
     def onExit(self, event):
 #        dialog = wx.MessageDialog(self, "Really close?", "Confirm Close", wx.OK | wx.CANCEL | wx.ICON_QUESTION)
