87180630977bf5afb854c7724636b7c709762865
Wil Langford <wil.langford+github@gmail.com>
2013-06-19 11:49:27 -0700

    Improved comments in poser.py.

--- FishFace/fishface/poser.py
+++ FishFace/fishface/poser.py
@@ -35,34 +35,50 @@ class Poser:
         startAngle = 0
         stopAngle = 180 + (int(180 / samples) * 4)
 
+        # iterations to refine the angle - we just care how many, not
+        # what iteration we're actually on
         for dummy in range(iterations):
             stepsize = int((stopAngle - startAngle) / samples)
+            # for each of the candidate angles, compute the rotated picture
+            # and the horizontal sums for each line of the rotated picture
             for angle in range(startAngle, stopAngle, stepsize):
                 self.rotate(angle)
                 self.horsum(angle)
 
+            # find the keys for each of the horizontal sum arrays
             ks = list(self.horsums.keys())
+            # find the maximum value from each horizontal sum array
             vs = [np.amax(hs) for hs in self.horsums.values()]
+            # and the maximum value of all of those maximums
             max_length = max(vs[2:-2])
+            # and the candidate will be the angle that produces that
+            # maximum value
             candidate = ks[vs.index(max_length)]
 
+            # at the next iteration, we'll start one stepsize before and
+            # end one stepsize after the current candidate, and repartition
+            # this new range with a new stepsize when the next iteration hits
             startAngle = max(0, candidate - stepsize)
             stopAngle = min(180 + (4 * stepsize), candidate + stepsize)
 
+        # after the specified number of iterations, we will rotate the
+        # current imageframe to the candidate angle
         frhor = self.rotate(candidate)
 
+        # find the horizontal middle of the candidate-rotated image
         mid_x = int(frhor.shape[0] / 2)
 
+        # find the sum of pixels on each side of that horizontal middle
         leftsum = np.sum(frhor[:, :mid_x])
         rightsum = np.sum(frhor[:, mid_x:])
 
+        # if the leftsum is less than the rightsum, then the fish is facing
+        # right and not left.  Rotate it 180 degrees so it faces left.
         if leftsum < rightsum:
             candidate = candidate + 180 % 360
 
         # The actual angle of the fish is the inverse of the candidate,
         # because we rotated the whole array and then measured along one axis.
-        # Also, because at this point we only know a line parallel to the long
-        # axis and not the orientation of that axis, 180 is as good as 360.
         return int((-candidate) % 360)
 
     def _rotate(self, degrees):
