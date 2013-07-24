1291399c74c2016caab63d20422c63c84e268f3e
Wil Langford <wil.langford+github@gmail.com>
2013-07-24 09:57:33 -0700

    Plugging away at marker recognition.

--- FishFace/marker-detection/marker.py
+++ FishFace/marker-detection/marker.py
@@ -1,4 +1,5 @@
 import copy
+import math
 
 try:
     import numpy as np
@@ -7,7 +8,6 @@ except ImportError:
     raise
 
 try:
-    import cv2.cv as cv
     import cv2
 except ImportError:
     print "Couldn't import OpenCV."
@@ -20,7 +20,7 @@ class Marker():
     def __init__(self):
         self.array = None
 
-    def marker1(self, bgColor=255, fgColor=0):
+    def setMarker1(self, bgColor=255, fgColor=0):
         self.array = np.ones((750,1000), dtype=np.uint8)*bgColor
 
         triangle = (np.array([[0,0], [300,150], [0,300]]) + (100,100)).astype('int32')
@@ -33,7 +33,7 @@ class Marker():
         cv2.fillPoly(self.array,[house],fgColor)
         cv2.fillPoly(self.array,[chevron],fgColor)
 
-    def marker2(self, bgColor=255, fgColor=0):
+    def setMarker2(self, bgColor=255, fgColor=0):
         self.array = np.ones((1200,1600), dtype=np.uint8)*bgColor
 
 
@@ -50,11 +50,11 @@ class Marker():
         cv2.fillPoly(self.array, [triangle1], fgColor)
         cv2.fillPoly(self.array, [triangle2], fgColor)
 
-    def marker3(self, bgColor=255, fgColor=0):
+    def setMarker3(self, bgColor=255, fgColor=0):
         self.array = np.ones((1200,1600), dtype=np.uint8)*bgColor
 
 
-        triangle1 = (np.array([[0,0], [100,200], [0,400]]) + (400,400)).astype('int32')
+        triangle1 = (np.array([[0,0], [100,100], [0,400]]) + (400,400)).astype('int32')
         triangle2 = (np.array([[0,400], [100,0], [100,400]]) + (1100,400)).astype('int32')
 
         border = [
@@ -68,51 +68,74 @@ class Marker():
         cv2.fillPoly(self.array, [triangle2], fgColor)
 
 
-class FindMarker():
-    def __init__(self, imageFrame):
-        self.frame = imageFrame
+class MarkerFinder():
+    def __init__(self):
+        pass
 
-    def findMarker3(self, threshold=30):
-        self.frame.applyGrayImage()
+    def findMarker3(self, frame, threshold=30, cropEdges=0.25, outerHullTolerance=50, innerPolyTolerance=5):
+        frame.applyGrayImage()
 
-        Y = self.frame.data['spatialShape'][0]
-        X = self.frame.data['spatialShape'][1]
+        Y = frame.data['spatialShape'][0]
+        X = frame.data['spatialShape'][1]
 
         # take every tenth pixel in the middle 60%x60% of the image...
-        colorSamples = self.frame.array[Y*0.2:Y*0.8:10, X*0.2:X*0.8:10]
+        colorSamples = frame.array[Y*cropEdges:Y*(1-cropEdges):10, X*cropEdges:X*(1-cropEdges):10]
 
         # ... and find the median of those values
         median = np.median(colorSamples)
 
-        ar = np.abs(self.frame.array.astype('int16') - median)
+        ar = cv2.threshold(cv2.absdiff(frame.array,median),threshold,255,cv2.THRESH_BINARY_INV)[1]
 
-        ar = cv2.threshold(ar.astype(np.uint8),threshold,255,cv2.THRESH_BINARY_INV)[1]
+        outerContours = cv2.findContours(ar.copy(), mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)[1][0]
 
-        ar2 = ar.astype(np.uint8)
+        outerHull = cv2.approxPolyDP(cv2.convexHull(outerContours),outerHullTolerance,True)
+        box = cv2.boxPoints(cv2.minAreaRect(outerHull))
+#        brec = cv2.boundingRect(box)
+#        innerar = 255 - ar[brec[1]:brec[1]+brec[3], brec[0]:brec[0]+brec[2]]
 
-        outer = cv2.findContours(ar, mode=cv.CV_RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)[0]
-
-        outer = cv2.approxPolyDP(outer[0],100,True)
-
-        outer = cv2.minAreaRect(outer)
-
-        return outer, ar2
-
-        # polygons = []
-        #
-        # for c in ctrs:
-        #     polygons.append(cv2.approxPolyDP(c,5,True))
-        #
-        # polygons = [[list(point[0]) for point in shape] for shape in polygons]
-        #
-        # polygons = [(polygon, len(polygon)) for polygon in polygons if len(polygon)>1]
-        #
-        # return polygons
+        # innerar = innerar[::-1]
 
 
+        allContours = cv2.findContours(ar.copy(), mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)[1]
+
+        polygons = []
+
+        for ctr in allContours:
+            polygons.append(cv2.approxPolyDP(ctr,innerPolyTolerance,True))
+
+        polygons = [[list(point[0]) for point in shape] for shape in polygons]
+
+        polygons = [(polygon, len(polygon)) for polygon in polygons if len(polygon)>1]
+
+        polygons.append([1,1])
+
+        triangles = [polygon[0] for polygon in polygons if polygon[1]==3]
+
+        # fr = imageframe.Frame(ar)
+        # fr.onScreen()
+
+        return box, triangles
 
 
 
+    def anglesFromTriangleVertices(self, verts):
+        verts = np.array(verts)
+
+        vects = []
+        for i in range(3):
+            vects.append(verts[i] - verts[(i+1)%3])
+
+        sideLengths = [math.hypot(x[0],x[1]) for x in vects]
+
+        def angleFromSides(a,b,c):
+            angle = math.acos((a**2 + b**2 - c**2)/(2*a*b))
+            return math.degrees(angle)
+
+        angles = []
+        for i in range(3):
+            angles.append(angleFromSides(sideLengths[i],sideLengths[(i+1)%3],sideLengths[(i+2)%3]))
+
+        return angles
 
 
 
