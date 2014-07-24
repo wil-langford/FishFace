d35ea0c4f7a482aafac650b421e593b459766649
wil-langford <wil.langford+github@gmail.com>
2014-07-24 14:08:33 -0700

    Increased delay to give pyro name server time to spin up.

--- FishFace/bin/raspberrypi/startup.sh
+++ FishFace/bin/raspberrypi/startup.sh
@@ -3,7 +3,7 @@
 export PYRO_SERIALIZERS_ACCEPTED=serpent,json,marshal,pickle
 
 # which address should the Pyro nameserver run on?
-PYRO_HOST_IP='192.168.0.1'
+PYRO_HOST_IP='mystaticip'
 
 # Session name argument
 SES_NAME='FishFaceStartup'
@@ -11,11 +11,8 @@ SES_NAME='FishFaceStartup'
 # start in detached mode
 /usr/bin/screen -dmS $SES_NAME /usr/bin/python -m Pyro4.naming --host=$PYRO_HOST_IP
 
-# set up the Pyro4 nameserver window
-# /usr/bin/screen -S $SES_NAME -p 0 -X exec /usr/bin/python -m Pyro4.naming --host=$PYRO_HOST_IP
-
-# wait 2 seconds to make sure Pyro has had time to start up
-sleep 2
+# wait 4 seconds to make sure Pyro has had time to start up
+sleep 4
 
 # set up the imagery server
 /usr/bin/screen -S $SES_NAME -X screen /usr/bin/python /home/pi/imageryserver.py
